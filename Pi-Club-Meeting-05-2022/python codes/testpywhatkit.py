39311670b07ab381db3d2f884499eead5190291d
ssgoh <iamssgoh@gmail.com>
2022-05-09 11:19:49 +0800

    Create testpywhatkit.py

--- Pi-Club-Meeting-05-2022/python codes/testpywhatkit.py
+++ Pi-Club-Meeting-05-2022/python codes/testpywhatkit.py
@@ -18,9 +18,15 @@ while True:
     datetime_now = datetime.datetime.now()  #this is will give us the current time
     #we need to add say 10 seconds for pywhatkit to send our msg out
     send_time = datetime_now + datetime.timedelta(seconds=60)
-    alert_phone_no="+65xxxxxxxx"
+    alert_phone_no="+6591080064"
     alert_msg="Garbage Bin No 12345 is full. Please clear it"
+    #The pywhatkit.sendwhatmsg function requires 4 basic parameters
+    #the destination phone no, the message content, the hour, the minute to send the message
+    #e.g. pywhatkit.sendwhatmsg('+6512345678','hello world',15,30 will send the message out at 15:30 hours
+    #send_time added a 60 seconds delay before whatsapp sends the message.  If time now is 15:29, adding 60 seconds will
+    #get the message out at 15:30, hence 15,30
     print(alert_phone_no, alert_msg, send_time.hour, send_time.minute,send_time.second)
+    
     pywhatkit.sendwhatmsg(alert_phone_no, alert_msg, send_time.hour, send_time.minute)
     sleep(60)
         
