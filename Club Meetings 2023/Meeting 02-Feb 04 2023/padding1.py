615d053bb0352ae651f3112a01c7c8772b2f8d9f
ssgoh <iamssgoh@gmail.com>
2023-02-01 15:17:51 +0800

    Add files via upload

---
+++ Club Meetings 2023/Meeting 02-Feb 04 2023/padding1.py
@@ -0,0 +1,5 @@
+s = "3"
+width = 2
+padded = "0"*(width - len(s)) + s
+print(padded)
+
---
+++ Club Meetings 2023/Meeting 02-Feb 04 2023/testwifi.py
@@ -0,0 +1,29 @@
+#Library
+from machine import Pin
+import network
+import ubinascii
+#Setup
+#LED to indicate that wifi is connected
+status_led= Pin(28, Pin.OUT)
+status_led.off()  #set it off initially
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+#Your wifi connection credentials here
+ssid = "ASUS"
+password = "Study090317"
+print('attempting to connect to wifi')
+wlan.connect(ssid, password)
+while True:
+    
+    if wlan.isconnected():
+        break
+status_led.on() 
+print("Wifi Connected" , wlan.ifconfig())
+mac = ubinascii.hexlify(wlan.config('mac'),':').decode()
+print('Mac Address ' , mac)
+# Other things you can query
+print('Channel',wlan.config('channel'))
+print('SSID',wlan.config('essid'))
+print('TX Power',wlan.config('txpower'))
