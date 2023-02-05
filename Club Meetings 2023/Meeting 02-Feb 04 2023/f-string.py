eebe20947ba4a866ff67ea8a413bf5312b82c32c
ssgoh <iamssgoh@gmail.com>
2023-02-05 11:07:31 +0800

    Add files via upload

---
+++ Club Meetings 2023/Meeting 02-Feb 04 2023/f-string.py
@@ -0,0 +1,23 @@
+#f-string
+
+temp=28
+humidity=80
+
+print('Using f-string')
+print(f"Temp : {temp} deg C")
+print(f"Humidity : {humidity} %")
+print("")
+print("")
+
+
+print('Using concactenantion')
+print('Temp : ' + str(temp) + ' deg C')
+print('Humidity : ' + str(humidity) + ' %')
+print("")
+print("")
+
+print('Advantage of f-string')
+print("Don't need to convert number value to string")
+
+
+
