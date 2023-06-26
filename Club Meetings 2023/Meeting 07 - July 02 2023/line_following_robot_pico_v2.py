cfea397da0a4e663ea4fdb3adc0e2ab60865a0d4
ssgoh <iamssgoh@gmail.com>
2023-06-26 10:58:04 +0800

    Add files via upload

---
+++ Club Meetings 2023/Meeting 07 - July 02 2023/line_following_robot_pico_v2.py
@@ -0,0 +1,102 @@
+from machine import Pin,PWM #importing PIN and PWM
+import time #importing time
+import utime
+
+
+status_led=Pin(17,Pin.OUT)
+
+# Defining motor pins on the L298N
+left_motor_enA=PWM(Pin(15))    #right wheel PWM - Defining enable pins and PWM object
+left_motor_forward_in1=Pin(14,Pin.OUT) #right wheel forward (clockwise)
+left_motor_backward_in2=Pin(13,Pin.OUT) #right wheel backward (anti-clockwise)
+
+right_motor_backward_in3=Pin(12,Pin.OUT) #left wheel forward (clockwise)
+right_motor_forward_in4=Pin(11,Pin.OUT) #left wheel backward (anti-clockwise)
+right_motor_enB=PWM(Pin(10))    #left wheel PWM - Defining enable pins and PWM object
+
+# Defining  right and left IR digital pins as input
+right_ir = Pin(2, Pin.IN)
+left_ir = Pin(3, Pin.IN)
+
+# Defining frequency for enable pins
+left_motor_enA.freq(1000)
+right_motor_enB.freq(1000)
+
+# Setting maximum duty cycle for maximum speed
+left_motor_enA.duty_u16(45535)
+right_motor_enB.duty_u16(45535)
+
+# Forward
+def move_forward():
+    left_motor_forward_in1.on()    #left wheel forward
+    left_motor_backward_in2.off()  #left wheel backward
+    right_motor_backward_in3.off() #right wheel backward
+    right_motor_forward_in4.on()   #right wheel forward
+    
+# Backward
+def move_backward():
+    left_motor_forward_in1.off()  #left wheel forward
+    left_motor_backward_in2.on()  #left wheel backward
+    right_motor_backward_in3.on() #right wheel backward
+    right_motor_forward_in4.off() #right wheel forward
+    
+#Turn Right
+def turn_right():
+    left_motor_forward_in1.on()    #left wheel forward
+    left_motor_backward_in2.off()  #left wheel backward
+    right_motor_backward_in3.off() #right wheel backward
+    right_motor_forward_in4.off()  #right wheel forward
+
+#Turn Left
+def turn_left():
+
+    left_motor_forward_in1.off()   #left wheel forward
+    left_motor_backward_in2.off()  #left wheel backward
+    right_motor_backward_in3.off() #right wheel backward
+    right_motor_forward_in4.on()   #right wheel forward
+
+   
+#Stop
+def stop():
+    left_motor_forward_in1.off()   #left wheel forward
+    left_motor_backward_in2.off()  #left wheel backward
+    right_motor_backward_in3.off() #right wheel backward
+    right_motor_forward_in4.off()  #right wheel forward
+
+def move(pin):
+    if status_led.value() == 0:
+        status_led.value(1)
+    elif status_led.value() == 1:
+        status_led.value(0)
+
+
+button =  Pin(16, Pin.IN, Pin.PULL_DOWN)
+button.irq(trigger=Pin.IRQ_RISING , handler=move)  
+
+status_led.off()
+
+while True:
+    if status_led.value() == 1:
+        right_val=right_ir.value() #Getting right IR value(0 or 1)
+        left_val=left_ir.value() #Getting left IR value(0 or 1)
+        
+        print('Right:',str(right_val)+"- Left:"+str(left_val))
+        
+        # Controlling robot direction based on IR value
+        if right_val==0 and left_val==0:
+            move_forward()
+        elif right_val==1 and left_val==0:
+            turn_right()
+        elif right_val==0 and left_val==1:
+            turn_left()
+        elif right_val == 0 and left_val == 0:
+            stop()
+        
+        
+    else:
+        stop()
+        status_led.off()
+    utime.sleep(0.005)
+ 
+ 
+ 
---
+++ Club Meetings 2023/Meeting 07 - July 02 2023/robby_with_class.py
@@ -0,0 +1,115 @@
+#Library
+from machine import Pin
+import network
+import ubinascii
+import socket
+from classRobotCar import robotcar
+from time import sleep
+
+#setup
+#this led's purpose is to tell us that wifi is connected
+#initially set to off
+wifi_status_led=Pin(21,Pin.OUT)
+wifi_status_led.off()
+
+#this led's purpose is to tell us that the Pico is powered up
+status_led = Pin(17, Pin.OUT)
+status_led.on()
+
+
+# Defining motor pins on the L298N
+robot = robotcar(15, 14, 13, 10, 12, 11)
+
+# Defining frequency for enable pins
+robot.left_motor_enA.freq(1000)
+robot.right_motor_enB.freq(1000)
+# Setting maximum duty cycle for maximum speed
+robot.left_motor_enA.duty_u16(45535)
+robot.right_motor_enB.duty_u16(45535)
+ 
+ 
+#show that pico is powered up
+status_led.on()
+wifi_status_led.on()
+sleep(2)
+status_led.off()
+wifi_status_led.off()
+sleep(2)
+
+
+connected_to_wifi=False
+ssid='VirusGenerator'
+password='VGAquarius090317'
+
+#connecting to wifi
+print('connecting to wifi...')
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+
+while connected_to_wifi == False:
+    
+    if wlan.isconnected():
+        connected_to_wifi = True
+        print(wlan.status(), ' wlan status 3 means connected ')
+        wifi_status_led.on()
+        
+print('Connected to :' ,wlan.ifconfig() )
+print('This Pico Server IP Address :' ,wlan.ifconfig()[0])
+mac = ubinascii.hexlify(wlan.config('mac'),':').decode()
+print('Mac Address ' , mac)
+
+
+html = open('robot.html', 'r').read()
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+#addr=('0.0.0.0',80)
+s = socket.socket()
+s.bind(addr)
+s.listen(5)
+print('listening on', addr)
+
+
+
+
+status_led.off()
+robot.stop()
+
+ 
+# Listen for connections, serve client
+while True:
+    
+    try:       
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        request = cl.recv(1024)
+        print("request:")
+        print(request)
+        request = str(request)
+        moveLeft = request.find('key=LEFT')
+        moveRight = request.find('key=RIGHT')
+        moveForward = request.find('key=FORWARD')
+        moveBackward = request.find('key=BACKWARD')
+        stop = request.find('key=STOP')
+        
+        if moveLeft ==8  :
+            robot.turn_left()
+        elif moveRight == 8  :
+            robot.turn_right()
+        elif moveForward == 8 :
+            robot.move_forward()
+        elif moveBackward == 8 :
+            robot.move_backward()
+        elif stop == 8 :
+            robot.stop()
+        
+        
+        response = html  
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(response)
+        cl.close()
+        
+    except OSError as e:
+        cl.close()
+        print('connection closed')
+        
+ 
