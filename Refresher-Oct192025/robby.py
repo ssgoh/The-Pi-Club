050f7b7e188ab75dc2490fce1d82ee0b3646ad25
ssgoh <iamssgoh@gmail.com>
2025-10-18 14:48:53 +0800

    Add files via upload

--- Refresher-Oct192025/robby.py
+++ Refresher-Oct192025/robby.py
@@ -2,167 +2,161 @@
 from machine import Pin, PWM, time_pulse_us
 from time import sleep, sleep_us
 from picozero import Button, LED
-
-start_button = Button(17)
-stop_button = Button(19)
-red_led = LED(7)
+button = Button(17)
+red_led=LED(18)
 red_led.off()
 
 # Define pins for hcsr04 ultrasonic sensor
 trigger = Pin(3, Pin.OUT)
 echo = Pin(2, Pin.IN)
 
-# Motor driver L298N pins
-speed_ENA = PWM(Pin(15))
-speed_ENA.freq(1000)
+#define pins for motor driver L298N
 IN1 = Pin(14, Pin.OUT)
 IN2 = Pin(13, Pin.OUT)
-
+speed_ENA = PWM(Pin(15))
+speed_ENA.freq(1000)
 
 IN3 = Pin(11, Pin.OUT)
 IN4 = Pin(12, Pin.OUT)
 speed_ENB = PWM(Pin(10))
 speed_ENB.freq(1000)
 
-# Servo Motor
+#define pins for Servo Motor
 servo_pwm = PWM(Pin(16))
 servo_pwm.freq(50)
 
+distance_to_obstance=0
+left_distance=0
+right_distance=0
 
+#get distance measured by the ultrasonic sensor
 def measure_distance():
+    # Send a 10µs pulse to trigger
     trigger.low()
     sleep_us(2)
     trigger.high()
     sleep_us(10)
     trigger.low()
-    duration = time_pulse_us(echo, 1, 30000)
-    if duration < 0:
-        return None
     
+    # Measure the duration of the echo pulse
+    duration = time_pulse_us(echo, 1, 30000)  # Timeout after 30ms
+    
+    # Calculate distance in cm (speed of sound = 343 m/s)
     distance = (duration * 0.0343) / 2
     return distance
 
 
+
+#set the rotation angle of the servo motor piggy backing the ultrasonic sensor
 def set_angle(angle):
+    # Convert angle (0–180) to duty cycle (500–2500 µs)
     pulse_width = 500 + (angle / 180) * 2000
     duty = int((pulse_width / 20000) * 65535)
     servo_pwm.duty_u16(duty)
 
 
-def stop():
-    IN1.low()
-    IN2.low()
-    IN3.low()
-    IN4.low()
-
-
-def backward(dc):
-    speed_ENA.duty_u16(dc)
-    speed_ENB.duty_u16(dc)
-    IN1.high()
-    IN2.low()
-    IN3.high()
-    IN4.low()
-
-
-def forward(dc):
-    speed_ENA.duty_u16(dc)
-    speed_ENB.duty_u16(dc)
-    IN1.low()
-    IN2.high()
-    IN3.low()
-    IN4.high()
-
-
-def turn_left(dc):
-    speed_ENA.duty_u16(dc)
-    speed_ENB.duty_u16(dc)
-    IN1.low()
-    IN2.low()
-    IN3.low()
-    IN4.high()
-
-
-def turn_right(dc):
-    speed_ENA.duty_u16(dc)
-    speed_ENB.duty_u16(dc)
-    IN1.low()
-    IN2.high()
-    IN3.low()
-    IN4.low()
-    
+#after an obstacle is detected the robot will find the best escape route
+#by checking if left side has more clearance or right side
+#direction change based on which side offers better clearance
 def get_best_direction():
-    left_distance = 0
-    right_distance = 0
-    for _ in range(3):
+    left_distance=0
+    right_distance=0
+    
+    
+    for x in range(1,4,1):
+        #get left position
         set_angle(135)
-        sleep(0.5)
-        dist = measure_distance()
+        sleep(1)
+        dist=measure_distance()
         if dist > left_distance:
             left_distance = dist
-
+        
         set_angle(45)
-        sleep(0.5)
-        dist = measure_distance()
+        sleep(1)
+        dist=measure_distance()
         if dist > right_distance:
             right_distance = dist
-        sleep(0.2)
-
+        
+        sleep(1)
     set_angle(90)
     return left_distance, right_distance
+     
+
+#motor control functions based on settings
+#made to the pins of the L298N
+def stop():
+    IN1.low()  #stop
+    IN2.low()
+    IN3.low()  #stop
+    IN4.low()     
+
+def forward(dutycycle):
+    speed_ENA.duty_u16(dutycycle)
+    IN1.low()  #spin forward
+    IN2.high()
+    speed_ENB.duty_u16(dutycycle)
+    IN3.low()  #spin forward
+    IN4.high()     
 
 
-# --- BUTTON ACTIONS ---
-def startRobot():
-    if red_led.value == 0:
-        red_led.on()
-        print("Robot started")
 
-def stopRobot():
-    if red_led.value == 1:
-        red_led.off()
-        stop()
-        print("Robot stopped")
-
-
-stop()
+def backward(dutycycle):
+    speed_ENA.duty_u16(dutycycle)
+    IN1.high()  #spin backward
+    IN2.low()
+    speed_ENB.duty_u16(dutycycle)
+    IN3.high()  #spin backward
+    IN4.low()     
+    
+def turn_left(dutycycle):
+    speed_ENA.duty_u16(dutycycle)
+    IN1.low()  #spin forward
+    IN2.low()
+    speed_ENB.duty_u16(dutycycle)
+    IN3.low()  #spin forward
+    IN4.high()     
+    
+def turn_right(dutycycle):
+    speed_ENA.duty_u16(dutycycle)
+    IN1.low()  #spin forward
+    IN2.low()
+    speed_ENB.duty_u16(dutycycle)
+    IN3.high()  #spin forward
+    IN4.low()     
  
 
 
-# --- MAIN CONTROL LOOP ---
-def run():
+#the robot program will start when the button is pressed
+def startRobot():
+    red_led.on()
+    forward(60000)
     while True:
-        if red_led.value == 1:  # Only run if LED is ON
-            distance = measure_distance()
-            print("Distance:", distance)
-
-            if distance <= 20:
-                backward(60000)
+        distance = measure_distance()
+        print(distance)
+        if distance <= 20:
+            backward(60000)
+            sleep(1)
+            stop()
+            left, right = get_best_direction()
+            if left > right:
+                turn_left(60000)
                 sleep(1)
-                stop()
-                left, right = get_best_direction()
-                if left > right:
-                    turn_left(60000)
-                else:
-                    turn_right(60000)
-                sleep(.3) #giving robot .3 sec to make the turn
-                set_angle(90)  #position ultrasonic sensor back to front position
+                set_angle(90)
                 forward(60000)
             else:
+                turn_right(60000)
+                sleep(1)
+                set_angle(90)
                 forward(60000)
         else:
-            stop()
-        sleep(0.05)  # Let the CPU rest briefly
+            pass
+        
+        
 
 
-# --- INITIALIZATION ---
-stop()
-set_angle(90)
-print("Ready. Press Start to begin.")
+#initialising 
+stop()  #stop all the motor functions
+set_angle(90)  #the eyes of robot facing front
+red_led.off()
 
-start_button.when_pressed = startRobot
-stop_button.when_pressed = stopRobot
-
-run()  # keep running main loop forever
-
- 
+button.when_pressed = startRobot
